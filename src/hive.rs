460fbb9bab73cc3d563b2574a9dcfc243843b150
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2016-11-14 20:21:15 -0500

    Finished hive validation logic.

--- src/hive.rs
+++ src/hive.rs
@@ -4,12 +4,20 @@ use std::error;
 use std::result;
 use std::fs::File;
 use std::path::Path;
+use std::io::Cursor;
+use std::io::SeekFrom;
 use std::io::prelude::*;
 
+use byteorder::{LittleEndian, ReadBytesExt};
+
+/// The HiveError type. Every sort of error is represented in this enum.
 #[derive(Debug)]
 pub enum HiveError {
+    /// The file cannot be opened. Carries with it the underlying io::Error.
     CannotOpenHive(io::Error),
+    /// Data could not be read. Carries with it the underlying io::Error.
     CannotReadData(io::Error),
+    /// The hive is invalid.
     InvalidHive
 }
 
@@ -49,21 +57,83 @@ pub struct Hive {
 }
 
 impl Hive {
+    /// Validates the registry hive as best it can, and constructs the Hive
+    /// object if all checks pass.
+    ///
+    /// This function checks the following in order to ensure a valid hive:
+    /// 1. Whether the file signature is correct
+    /// 2. Whether the primary and secondary sequence numbers are synchronized
+    /// 3. Whether the header checksum is valid
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hive::Hive;
+    ///
+    /// let h = Hive::open("/path/to/hive/file");
+    /// ```
+    ///
+    /// # Errors
+    /// Will return with the appropriate error if the file cannot be opened, if
+    /// data could not be read, or if the file is not a valid registry hive.
+    ///
     pub fn new<P: AsRef<Path>>(path: P) -> result::Result<Hive, HiveError> {
         // open file
         let mut reg_file = try!(File::open(path.as_ref())
                       .map_err(HiveError::CannotOpenHive));
 
-        // validate header as best we can
-        let mut check = [0; 4];
-        let correct_header: [u8; 4] = [0x72, 0x65, 0x67, 0x66]; //"regf" ascii chars
+        // check file signature
+        let mut file_sig = [0; 4];
+        let actual_sig: [u8; 4] = [0x72, 0x65, 0x67, 0x66]; //"regf" ascii chars
 
-        try!(reg_file.read_exact(&mut check)
+        try!(reg_file.read_exact(&mut file_sig)
              .map_err(HiveError::CannotReadData));
 
-        if !check.eq(&correct_header) {
+        if !file_sig.eq(&actual_sig) {
             return Err(HiveError::InvalidHive);
         }
+
+        // check sequence numbers
+        let mut primary = [0; 4];
+        let mut secondary = [0; 4];
+        try!(reg_file.read_exact(&mut primary)
+             .map_err(HiveError::CannotReadData));
+        try!(reg_file.read_exact(&mut secondary).
+             map_err(HiveError::CannotReadData));
+
+        if !primary.eq(&secondary) {
+            return Err(HiveError::InvalidHive);
+        }
+
+        // do the XOR checksum
+        let mut header_raw = [0; 508];
+        let mut header_check = [0; 4];
+        let mut xor: u32 = 0;
+
+        try!(reg_file.seek(SeekFrom::Start(0))
+             .map_err(HiveError::CannotReadData));
+        try!(reg_file.read_exact(&mut header_raw)
+             .map_err(HiveError::CannotReadData));
+        try!(reg_file.read_exact(&mut header_check)
+             .map_err(HiveError::CannotReadData));
+
+        let mut header_rdr = Cursor::new(header_raw.to_vec());
+        let mut check_rdr = Cursor::new(header_check);
+
+        let csum = check_rdr.read_u32::<LittleEndian>().unwrap();
+
+        for _ in 0..127 { // 508 / 4, the number of u32s in the vec
+            xor ^= try!(header_rdr.read_u32::<LittleEndian>()
+                        .map_err(HiveError::CannotReadData));
+        }
+
+        if xor != csum {
+            return Err(HiveError::InvalidHive);
+        }
+
+        // reset the seek ptr to 0 before instantiating
+        try!(reg_file.seek(SeekFrom::Start(0))
+             .map_err(HiveError::CannotReadData));
 
         Ok(Hive {f: reg_file})
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate byteorder;
 mod hive;
 
 use std::env;
